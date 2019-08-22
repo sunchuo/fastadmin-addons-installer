bfd7ad8669abd96d568db07f7e16e0bf25249044
BT <btniao@gmail.com>
2019-08-22 14:54:32 +0800

    fix

---
+++ src/Plugin.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Fastadmin\AddonsInstaller;
+
+use Composer\Composer;
+use Composer\IO\IOInterface;
+use Composer\Plugin\PluginInterface;
+
+class Plugin implements PluginInterface
+{
+
+    public function activate(Composer $composer, IOInterface $io)
+    {
+        $installer = new Installer($io, $composer);
+        $composer->getInstallationManager()->addInstaller( $installer );
+
+
+        $installer = new Installer($io, $composer);
+        $composer->getInstallationManager()->addInstaller($installer);
+
+        if ($installer->supports($composer->getPackage()->getType())) {
+            $installer->addAddons($composer->getPackage(), true);
+        }
+    }
+}
