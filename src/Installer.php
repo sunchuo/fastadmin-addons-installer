b51e7106dff6ae78fe9adbe6792b76a61171cd85
BT <btniao@gmail.com>
2019-08-22 23:31:33 +0800

    fix

--- src/Installer.php
+++ src/Installer.php
@@ -10,6 +10,7 @@ use Composer\Package\PackageInterface;
 use Composer\Repository\InstalledRepositoryInterface;
 use Composer\Util\Filesystem;
 use think\addons\Service;
+use think\App;
 
 
 class Installer extends LibraryInstaller
@@ -21,17 +22,27 @@ class Installer extends LibraryInstaller
 
     public function __construct(IOInterface $io, Composer $composer, $type = 'library', Filesystem $filesystem = null, BinaryInstaller $binaryInstaller = null)
     {
-        //加载项目基础文件
-        $vendor_dir = $composer->getConfig()->get('vendor-dir').'/';
-        $app_path = realpath($vendor_dir.'../application/').'/';
-        echo 'app_path='.$app_path."\n";
-        define('APP_PATH', $app_path);
-
-        includeIfExists($vendor_dir.'autoload.php');
-        includeIfExists(APP_PATH.'../thinkphp/base.php');
-
         parent::__construct($io, $composer, $type, $filesystem, $binaryInstaller);
     }
+
+
+    public function initBase()
+    {
+        //加载项目基础文件
+        $this->io->write('加载项目基础文件');
+        !defined('APP_PATH') && define('APP_PATH', realpath($this->vendorDir.'../application/').'/');
+
+        $base_file = APP_PATH . '../thinkphp/base.php';
+        if (file_exists($base_file)) {
+            include_once $base_file;
+            if (!empty(App::initCommon())) {
+                $this->io->write('已加载');
+            } else {
+                throw new \Exception('加载失败');
+            }
+        }
+    }
+
 
     public function getInstallPath(PackageInterface $package)
     {
@@ -40,11 +51,8 @@ class Installer extends LibraryInstaller
             throw new InvalidAddonsException($package, '类型不支持');
         }
         $base = $this->packageTypes[$type];
-        $extra = $package->getExtra();
-        if (empty($extra['name'])) {
-            throw new InvalidAddonsException($package, '插件没定义名称');
-        }
-        return $base.$extra['name'];
+        $name = $this->getExtraName($package);
+        return $base.$name;
     }
 
     public function supports($packageType)
@@ -52,427 +60,79 @@ class Installer extends LibraryInstaller
         return array_key_exists($packageType, $this->packageTypes);
     }
 
-    public function addAddons(PackageInterface $package, $isRoot = false)
+
+    public function getExtraName(PackageInterface $package)
     {
-
-    }
-
-    public function update(InstalledRepositoryInterface $repo, PackageInterface $initial, PackageInterface $target)
-    {
-
-        echo 'update!!!'."\n";
-
-        Service::disable('cms');
-        // Update the plugin in vendor/ like a normal Composer library
-        parent::update($repo, $initial, $target);
-
-        Service::enable('cms');
-
+        $extra = $package->getExtra();
+        if (empty($extra['name']) || !preg_match('/^[a-z][a-z0-9]+$/', $extra['name'])) {
+            throw new InvalidAddonsException($package, '此插件没定义合法的插件名称');
+        }
+        return $extra['name'];
     }
 
 
 
     public function install(InstalledRepositoryInterface $repo, PackageInterface $package)
     {
-        echo 'install!!!'."\n";
+        $this->initBase();
+
+        $name = $this->getExtraName($package);
+        $this->io->write('正在安装插件:'.$name.' <'.$package->getPrettyName().'>');
+        $this->io->write('安装目录:'.$this->getInstallPath($package));
 
         parent::install($repo, $package);
-        Service::install('cms', false);
-        Service::enable('cms');
 
+        $this->io->write('安装完成');
+        $this->io->write('检查插件是否完整');
 
-//
-//        try {
-//            $this->addAddons($package);
-//        } catch (InvalidAddonsException $e) {
-//            parent::uninstall($repo, $package);
-//            throw $e;
-//        }
+        if (Service::check($name)) {
+            $this->io->write('完整');
+        }
+
+        $this->io->write('启用插件');
+        if (Service::enable($name)) {
+            $this->io->write('已启用');
+        }
     }
 
 
+    public function update(InstalledRepositoryInterface $repo, PackageInterface $initial, PackageInterface $target)
+    {
+        $this->initBase();
+
+        $name = $this->getExtraName($initial);
+        $this->io->write('正在更新插件:'.$name.' <'.$initial->getPrettyName().'>');
+        $this->io->write('禁用插件');
+        if (Service::disable($name)) {
+            $this->io->write('已禁用');
+        }
+
+        parent::update($repo, $initial, $target);
+        $this->io->write('更新完成');
+        $this->io->write('启用插件');
+        if (Service::enable($name)) {
+            $this->io->write('已启用');
+        }
+    }
 
 
     public function uninstall(InstalledRepositoryInterface $repo, PackageInterface $package)
     {
-        echo 'uninstall!!!'."\n";
-        Service::disable('cms');
-        Service::uninstall('cms');
-        // Uninstall the plugin from vendor/ like a normal Composer library
+        $this->initBase();
+        $name = $this->getExtraName($package);
+        $this->io->write('正在卸载插件:'.$name.' <'.$package->getPrettyName().'>');
+        $this->io->write('禁用插件');
+        if (Service::disable($name)) {
+            $this->io->write('已禁用');
+        }
+
+        $this->io->write('卸载插件');
+        if (Service::uninstall($name)) {
+            $this->io->write('已卸载');
+        }
+        $this->io->write('清空目录');
         parent::uninstall($repo, $package);
-
-//        // Remove the plugin info from plugins.php
-//        $this->removePlugin($package);
+        $this->io->write('已清空');
     }
-//
-//    /**
-//     * @param PackageInterface $package
-//     * @param bool $isRoot
-//     * @throws InvalidPluginException() if there's an issue with the plugin
-//     */
-//    public function addPlugin(PackageInterface $package, $isRoot = false)
-//    {
-//        $extra = $package->getExtra();
-//        $prettyName = $package->getPrettyName();
-//
-//        // Find the PSR-4 autoload aliases, the primary Plugin class, and base path
-//        $class = isset($extra['class']) ? $extra['class'] : null;
-//        $basePath = isset($extra['basePath']) ? $extra['basePath'] : null;
-//        $aliases = $this->generateDefaultAliases($package, $class, $basePath, $isRoot);
-//
-//        // class + basePath (required)
-//        if ($class === null) {
-//            throw new InvalidPluginException($package, 'Unable to determine the Plugin class');
-//        }
-//
-//        if ($basePath === null) {
-//            throw new InvalidPluginException($package, 'Unable to determine the base path');
-//        }
-//
-//        // handle (required)
-//        if (!isset($extra['handle']) || !preg_match('/^[a-zA-Z][\w\-]*$/', $extra['handle'])) {
-//            throw new InvalidPluginException($package, 'Invalid or missing plugin handle');
-//        }
-//
-//        // normalize the handle (copied from craft\services\Plugins::_normalizeHandle())
-//        $handle = $extra['handle'];
-//        if (strtolower($handle) !== $handle) {
-//            // copied from yii\helpers\BaseInflector::camel2id() w/ default options
-//            $handle = strtolower(trim(str_replace('_', '-', preg_replace('/(?<![A-Z])[A-Z]/', '-' . '\0', $handle)), '-'));
-//            // prevent double -'s
-//            $handle = preg_replace('/\-{2,}/', '-', $handle);
-//            // complain about it
-//            $this->io->write('<warning>' . $prettyName . ' uses the old plugin handle format ("' . $extra['handle'] . '"). It should be "' . $handle . '".</warning>');
-//        }
-//
-//        $plugin = array(
-//            'class' => $class,
-//            'basePath' => $basePath,
-//            'handle' => $handle,
-//        );
-//
-//        if ($aliases) {
-//            $plugin['aliases'] = $aliases;
-//        }
-//
-//        if (strpos($prettyName, '/') !== false) {
-//            list($vendor, $name) = explode('/', $prettyName);
-//        } else {
-//            $vendor = null;
-//            $name = $prettyName;
-//        }
-//
-//        // name
-//        if (isset($extra['name'])) {
-//            $plugin['name'] = $extra['name'];
-//        } else {
-//            $plugin['name'] = $name;
-//        }
-//
-//        // version
-//        if (isset($extra['version'])) {
-//            $plugin['version'] = $extra['version'];
-//        } else {
-//            $plugin['version'] = $package->getPrettyVersion();
-//        }
-//
-//        // schemaVersion
-//        if (isset($extra['schemaVersion'])) {
-//            $plugin['schemaVersion'] = $extra['schemaVersion'];
-//        }
-//
-//        // description
-//        if (isset($extra['description'])) {
-//            $plugin['description'] = $extra['description'];
-//        } else if ($package instanceof CompletePackageInterface && ($description = $package->getDescription())) {
-//            $plugin['description'] = $description;
-//        }
-//
-//        // developer
-//        if (isset($extra['developer'])) {
-//            $plugin['developer'] = $extra['developer'];
-//        } else if ($authorName = $this->getAuthorProperty($package, 'name')) {
-//            $plugin['developer'] = $authorName;
-//        } else if ($vendor !== null) {
-//            $plugin['developer'] = $vendor;
-//        }
-//
-//        // developerUrl
-//        if (isset($extra['developerUrl'])) {
-//            $plugin['developerUrl'] = $extra['developerUrl'];
-//        } else if ($package instanceof CompletePackageInterface && ($homepage = $package->getHomepage())) {
-//            $plugin['developerUrl'] = $homepage;
-//        } else if ($authorHomepage = $this->getAuthorProperty($package, 'homepage')) {
-//            $plugin['developerUrl'] = $authorHomepage;
-//        }
-//
-//        // developerEmail
-//        if (isset($extra['developerEmail'])) {
-//            $plugin['developerEmail'] = $extra['developerEmail'];
-//        } else if ($package instanceof CompletePackageInterface && ($support = $package->getSupport()) && isset($support['email'])) {
-//            $plugin['developerEmail'] = $support['email'];
-//        }
-//
-//        // documentationUrl
-//        if (isset($extra['documentationUrl'])) {
-//            $plugin['documentationUrl'] = $extra['documentationUrl'];
-//        } else if ($package instanceof CompletePackageInterface && ($support = $package->getSupport()) && isset($support['docs'])) {
-//            $plugin['documentationUrl'] = $support['docs'];
-//        }
-//
-//        // changelogUrl
-//        // todo: check $extra['support']['changelog'] if that becomes a thing - https://github.com/composer/composer/issues/6079
-//        if (isset($extra['changelogUrl'])) {
-//            $plugin['changelogUrl'] = $extra['changelogUrl'];
-//        }
-//
-//        // downloadUrl
-//        if (isset($extra['downloadUrl'])) {
-//            $plugin['downloadUrl'] = $extra['downloadUrl'];
-//        }
-//
-//        // t9nCategory
-//        if (isset($extra['t9nCategory'])) {
-//            $plugin['t9nCategory'] = $extra['t9nCategory'];
-//        }
-//
-//        // sourceLanguage
-//        if (isset($extra['sourceLanguage'])) {
-//            $plugin['sourceLanguage'] = $extra['sourceLanguage'];
-//        }
-//
-//        // hasCpSettings
-//        if (isset($extra['hasCpSettings'])) {
-//            $plugin['hasCpSettings'] = (bool)$extra['hasCpSettings'];
-//        }
-//
-//        // hasCpSection
-//        if (isset($extra['hasCpSection'])) {
-//            $plugin['hasCpSection'] = (bool)$extra['hasCpSection'];
-//        }
-//
-//        // components
-//        if (isset($extra['components'])) {
-//            $plugin['components'] = $extra['components'];
-//        }
-//
-//        // modules
-//        if (isset($extra['modules'])) {
-//            $plugin['modules'] = $extra['modules'];
-//        }
-//
-//        // minimum required version
-//        if (isset($extra['minVersionRequired'])) {
-//            $plugin['minVersionRequired'] = $extra['minVersionRequired'];
-//        }
-//
-//        $this->registerPlugin($package->getName(), $plugin);
-//    }
-//
-//    /**
-//     * @param string $name The plugin's package name
-//     * @param array $plugin The plugin config
-//     */
-//    protected function registerPlugin($name, array $plugin)
-//    {
-//        $plugins = $this->loadPlugins();
-//        $plugins[$name] = $plugin;
-//        $this->savePlugins($plugins);
-//    }
-//
-//    /**
-//     * @param PackageInterface $package
-//     * @param $class
-//     * @param $basePath
-//     * @param bool $isRoot
-//     * @return array|null
-//     */
-//    protected function generateDefaultAliases(PackageInterface $package, &$class, &$basePath, $isRoot)
-//    {
-//        $autoload = $package->getAutoload();
-//
-//        if (empty($autoload['psr-4'])) {
-//            return null;
-//        }
-//
-//        $fs = new Filesystem();
-//        $vendorDir = $fs->normalizePath($this->vendorDir);
-//        $aliases = array();
-//
-//        foreach ($autoload['psr-4'] as $namespace => $path) {
-//            if (is_array($path)) {
-//                // Yii doesn't support aliases that point to multiple base paths
-//                continue;
-//            }
-//
-//            // Normalize $path to an absolute path
-//            $cwd = $fs->normalizePath(getcwd());
-//            if (!$fs->isAbsolutePath($path)) {
-//                if ($isRoot) {
-//                    $path = $cwd . '/' . $path;
-//                } else {
-//                    $path = $this->vendorDir . '/' . $package->getPrettyName() . '/' . $path;
-//                }
-//            }
-//
-//            $path = $fs->normalizePath($path);
-//            $alias = '@' . str_replace('\\', '/', trim($namespace, '\\'));
-//
-//            if (strpos($path . '/', $vendorDir . '/') === 0) {
-//                $aliases[$alias] = '<vendor-dir>' . substr($path, strlen($vendorDir));
-//            } else if (strpos($path . '/', $cwd . '/') === 0) {
-//                $aliases[$alias] = '<root-dir>' . substr($path, strlen($cwd));
-//            } else {
-//                $aliases[$alias] = $path;
-//            }
-//
-//            // If we're still looking for the primary Plugin class, see if it's in here
-//            if ($class === null && file_exists($path . '/Plugin.php')) {
-//                $class = $namespace . 'Plugin';
-//            }
-//
-//            // If we're still looking for the base path but we know the primary Plugin class,
-//            // see if the class namespace matches up, and the file is in here.
-//            // If so, set the base path to whatever directory contains the plugin class.
-//            if ($basePath === null && $class !== null) {
-//                $n = strlen($namespace);
-//                if (strncmp($namespace, $class, $n) === 0) {
-//                    $testClassPath = $path . '/' . str_replace('\\', '/', substr($class, $n)) . '.php';
-//                    if (file_exists($testClassPath)) {
-//                        $basePath = dirname($testClassPath);
-//                        // If the base path starts with the vendor dir path, swap with <vendor-dir>
-//                        if (strpos($basePath . '/', $vendorDir . '/') === 0) {
-//                            $basePath = '<vendor-dir>' . substr($basePath, strlen($vendorDir));
-//                        }
-//                    }
-//                }
-//            }
-//        }
-//
-//        return $aliases;
-//    }
-//
-//    /**
-//     * @param PackageInterface $package
-//     * @param string $property
-//     *
-//     * @return null
-//     */
-//    protected function getAuthorProperty(PackageInterface $package, $property)
-//    {
-//        if (!$package instanceof CompletePackageInterface) {
-//            return null;
-//        }
-//
-//        $authors = $package->getAuthors();
-//        if (empty($authors)) {
-//            return null;
-//        }
-//
-//        $firstAuthor = reset($authors);
-//
-//        if (!isset($firstAuthor[$property])) {
-//            return null;
-//        }
-//
-//        return $firstAuthor[$property];
-//    }
-//
-//    /**
-//     * @param PackageInterface $package
-//     *
-//     * @return array|null The removed plugin info, or null if it wasn't there in the first place
-//     */
-//    protected function removePlugin(PackageInterface $package)
-//    {
-//        return $this->unregisterPlugin($package->getName());
-//    }
-//
-//    /**
-//     * @param string $name The plugin's package name
-//     *
-//     * @return array|null The removed plugin info, or null if it wasn't there in the first place
-//     */
-//    protected function unregisterPlugin($name)
-//    {
-//        $plugins = $this->loadPlugins();
-//
-//        if (!isset($plugins[$name])) {
-//            return null;
-//        }
-//
-//        $plugin = $plugins[$name];
-//        unset($plugins[$name]);
-//        $this->savePlugins($plugins);
-//        return $plugin;
-//    }
-//
-//    /**
-//     * @return array|mixed
-//     */
-//    protected function loadPlugins()
-//    {
-//        $file = $this->vendorDir . '/' . static::PLUGINS_FILE;
-//
-//        if (!is_file($file)) {
-//            return array();
-//        }
-//
-//        // Invalidate opcache of plugins.php if it exists
-//        if (function_exists('opcache_invalidate')) {
-//            @opcache_invalidate($file, true);
-//        }
-//
-//        /** @var array $plugins */
-//        $plugins = require($file);
-//
-//        // Swap absolute paths with <vendor-dir> tags
-//        $vendorDir = str_replace('\\', '/', $this->vendorDir);
-//        $n = strlen($vendorDir);
-//
-//        foreach ($plugins as &$plugin) {
-//            // basePath
-//            if (isset($plugin['basePath'])) {
-//                $path = str_replace('\\', '/', $plugin['basePath']);
-//                if (strpos($path . '/', $vendorDir . '/') === 0) {
-//                    $plugin['basePath'] = '<vendor-dir>' . substr($path, $n);
-//                }
-//            }
-//            // aliases
-//            if (isset($plugin['aliases'])) {
-//                foreach ($plugin['aliases'] as $alias => $path) {
-//                    $path = str_replace('\\', '/', $path);
-//                    if (strpos($path . '/', $vendorDir . '/') === 0) {
-//                        $plugin['aliases'][$alias] = '<vendor-dir>' . substr($path, $n);
-//                    }
-//                }
-//            }
-//        }
-//
-//        return $plugins;
-//    }
-//
-//    /**
-//     * @param array $plugins
-//     */
-//    protected function savePlugins(array $plugins)
-//    {
-//        $file = $this->vendorDir . '/' . static::PLUGINS_FILE;
-//
-//        if (!file_exists(dirname($file))) {
-//            mkdir(dirname($file), 0777, true);
-//        }
-//
-//        $array = str_replace(array("'<vendor-dir>", "'<root-dir>"), array('$vendorDir . \'', '$rootDir . \''), var_export($plugins, true));
-//        $fs = new Filesystem();
-//        file_put_contents($file, "<?php\n\n\$vendorDir = dirname(__DIR__);\n" .
-//            "\$rootDir = " . $fs->findShortestPathCode($this->vendorDir . '/craftcms', getcwd(), true) . ";\n\n" .
-//            "return $array;\n");
-//
-//        // Invalidate opcache of plugins.php if it exists
-//        if (function_exists('opcache_invalidate')) {
-//            @opcache_invalidate($file, true);
-//        }
-//    }
-
 }
+
